49fe2646e955ab2ee4de3fea640d8fae970bf9ce
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-03-27 18:50:25 +0400

    InceptionComponentAddedAtMLUI

---
+++ universe/apps/my-learn-ui/src/app/inception/inception.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'universe-inception',
+  templateUrl: './inception.component.html'
+})
+export class InceptionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
